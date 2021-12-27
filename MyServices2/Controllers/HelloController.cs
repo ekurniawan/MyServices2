368861bc289fea153ef845159480c3067aa3ac21
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-27 16:58:39 +0700

    Add project files.

---
+++ MyServices2/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyServices2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET: api/<HelloController>
+        [Authorize]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "erick", "budi" };
+        }
+
+        // GET api/<HelloController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+      
+    }
+}
---
+++ MyServices2/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace MyServices2.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
